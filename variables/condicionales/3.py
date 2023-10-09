5d572bc9c87ca11988d2b6c4306f2844cd2310bc
osmangonzale <125897300+osmangonzale@users.noreply.github.com>
2023-10-08 21:47:47 -0500

    Delete variables/condicionales directory

--- variables/condicionales/3.py
+++
@@ -1,8 +0,0 @@
-numero = float(input("Ingrese un número: "))
-
-if numero < 0:
-    print("El número ingresado es negativo.")
-elif numero > 0:
-    print("El número ingresado es positivo.")
-else:
-    print("El número ingresado es igual a cero.")
